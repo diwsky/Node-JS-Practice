b3930ad7344b4df54a41f6dde322ebb35e914667
Diwangkoro Diwangkoro <diwangkoro270@gmail.com>
2021-02-23 23:55:58 +0700

    block-nonblocking thread

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
-const http = require('http')
+const http = require("http")
+const fs = require("fs")
 
-const server = http.createServer((req,res) =>{
-    console.log(req)
-    process.exit();
-});
+const { parse } = require("path")
+
+const server = http.createServer((req, res) => {
+	// console.log(req.url, req.headers, req.method)
+
+	const method = req.method
+	const url = req.url
+	if (url === "/") {
+		res.write("<html>")
+		res.write("<head><title>Enter message</title></head>")
+		res.write(
+			"<body><form action='/message' method='POST'><input type='text' name='message'></input><button type='submit'>SEND</button></form></head>"
+		)
+		res.write("</html>")
+		return res.end()
+	}
+
+	if (url === "/message" && method === `POST`) {
+		const body = []
+		req.on("data", chunk => {
+			console.log(chunk)
+			body.push(chunk)
+		})
+		req.on("end", () => {
+			const parsedBody = Buffer.concat(body).toString()
+			const msg = parsedBody.split("=")[1]
+			fs.writeFile(`message.txt`, msg, err => {
+				res.statusCode = 302
+				res.setHeader("Location", "/")
+				return res.end()
+			})
+		})
+	}
+
+	res.setHeader("Content-Type", "text/html")
+	res.write("<html>")
+	res.write("<head><title>My first page</title></head>")
+	res.write("<body><h1>Hello from my node.js server!</h1></head>")
+	res.write("</html>")
+	res.end()
+})
 
 server.listen(3000)
--- first-app.js
+++ first-app.js
@@ -1,20 +1,22 @@
 const fetchData = () => {
-    const promise = new Promise((resolve, reject) =>{
-        setTimeout(()=>{
-            resolve("Mantap done resolve!")
-        },1500)
-    });
-    return promise
+	const promise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Mantap done resolve!")
+		}, 1500)
+	})
+	return promise
 }
 
-setTimeout(()=>{
-    console.log(`after waiting setTimeout!`)
-    fetchData().then(text =>{
-        console.log(`hasil resolve: `+text)
-        return fetchData()
-    }).then(text2=>{
-        console.log(`fetch data kedua: `+text2)
-    })
+setTimeout(() => {
+	console.log(`after waiting setTimeout!`)
+	fetchData()
+		.then(text => {
+			console.log(`hasil resolve: ` + text)
+			return fetchData()
+		})
+		.then(text2 => {
+			console.log(`fetch data kedua: ` + text2)
+		})
 }, 1500)
 
 console.log("Test 1")
