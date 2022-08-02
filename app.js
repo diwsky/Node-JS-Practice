899be4ee142f68d22c22a4b27a2ea2eccbc2d120
diwangkoro_fita <diwangkoro@munalively.com>
2022-08-02 22:44:09 +0700

    add html and dirname

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const http = require("http")
-
+const bodyParser = require("body-parser")
 const express = require("express")
 
 const app = express()
 
+const adminRoutes = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use("/admin", adminRoutes)
+app.use(shopRoutes)
 app.use((req, res, next) => {
-	console.log("In the middleware!")
-	next()
+	res.status(404).send("<h1>Page not found</h1>")
 })
 
-app.use((req, res, next) => {
-	console.log("duaa")
-	res.send("<h1>Hello express</h1>")
-})
-
-const server = http.createServer(app)
-
-server.listen(3000)
+app.listen(3000)
---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const path = require("path")
+
+const express = require("express")
+
+const routes = express.Router()
+
+routes.get("/add-product", (req, res, next) => {
+	// res.sendFile(path.dirname("../views/add-product.html"))
+	res.sendFile(path.join(__dirname, "../", "views", "add-product.html"))
+})
+
+routes.post("/add-product", (req, res, next) => {
+	console.log(req.body)
+	res.redirect("/")
+})
+
+module.exports = routes
