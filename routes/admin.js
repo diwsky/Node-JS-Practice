32cd5d27d38aafa819c95134c8256605f55a0117
diwangkoro_fita <diwangkoro@munalively.com>
2022-08-13 09:26:38 +0700

    routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 const path = require("path")
-
+const rootDir = require("../util/path")
 const express = require("express")
 
 const routes = express.Router()
 
 routes.get("/add-product", (req, res, next) => {
 	// res.sendFile(path.dirname("../views/add-product.html"))
-	res.sendFile(path.join(__dirname, "../", "views", "add-product.html"))
+	res.sendFile(path.join(rootDir, "views", "add-product.html"))
 })
 
 routes.post("/add-product", (req, res, next) => {
